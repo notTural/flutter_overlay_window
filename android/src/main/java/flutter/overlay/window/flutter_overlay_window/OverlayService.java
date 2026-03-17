9407b970caff8d4b0335c44ac6eacd162de6b480
notTural <rahman.trl.mammadov@gmail.com>
2026-03-17 13:26:46 +0100

    update: proejct bundle id

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -141,7 +141,7 @@ public class OverlayService extends Service implements View.OnTouchListener {
                 boolean enableDrag = call.argument("enableDrag");
                 resizeOverlay(width, height, enableDrag, result);
             } else if (call.method.equals("openMainApp")) {
-                intent.setClassName("com.hertzaz.hertz_driver", "com.hertzaz.hertz_driver.MainActivity");
+                intent.setClassName("com.hertzaz.driver", "com.hertzaz.driver.MainActivity");
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             }
